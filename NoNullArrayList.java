a80c49462079bb94405a94d9bc9c53bca0a2716c
ryan-li20 <rli20@stuy.edu>
2020-12-12 22:30:20 -0500

    working NoNull

---
+++ NoNullArrayList.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class NoNullArrayList<T> extends ArrayList<T>{
+  private ArrayList<T> s;
+
+  public NoNullArrayList(){
+    ArrayList<T> s = new ArrayList<T>();
+  }
+
+  public NoNullArrayList(int initialCapacity){
+    ArrayList<T> s = new ArrayList<T>(initialCapacity);
+  }
+
+  public T set(int index, T element){
+    remove(index);
+    add(element);
+    return element;
+  }
+
+  public boolean add(T element){
+    if(element != null){
+      super.add(element);
+      return true;
+    }
+    else{
+      throw new IllegalArgumentException("Added element cannot be null");
+    }
+  }
+
+  public void add(int index, T element){
+    if(element != null){
+      super.add(element);
+    }
+    else{
+      throw new IllegalArgumentException("Added element cannot be null");
+    }
+  }
+}
