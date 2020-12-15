40ca50e3bf0f90cb8326c3a206145aa3ede7210d
ryan-li20 <rli20@stuy.edu>
2020-12-15 00:15:26 -0500

    omigod it works finally

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -3,33 +3,111 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   private NoNullArrayList<T> ar;
 
   public OrderedArrayList(){
-    ar = new NoNullArrayList<T>();
+    NoNullArrayList<T> ar = new NoNullArrayList<T>();
   }
 
   public OrderedArrayList(int starting){
-    ar = new NoNullArrayList<T>(starting);
+    NoNullArrayList<T> ar = new NoNullArrayList<T>(starting);
   }
 
+
   public boolean add(T element){
-    if(ar.size() == 0){
-      return super.add(element);
+    if(element == null){
+      throw new IllegalArgumentException("not allowed >:(");
     }
-    if(ar.size() == 1){
-      if(ar.get(0).compareTo(element) >= 0){
-        return super.add(element);
+    if(super.size() == 0){
+      super.add(element);
+      System.out.println("no fukcing way");
+      return true;
+    }
+
+
+    if(super.size() == 1){
+      if(element.compareTo(super.get(0)) >= 0){
+        super.add(element);
+        System.out.println("monkey");
+        System.out.println(element);
+        return true;
       }
       else{
         super.add(0, element);
+        System.out.println("fucking everyhting");
         return true;
       }
     }
-    for(int i = 0; i < ar.size()-1; i++){
-      if(ar.get(i+1).compareTo(element) > 0 && ar.get(i).compareTo(element) <= 0){
-        super.add(i, element);
+
+
+    if(super.size() > 1){
+
+      if(element.compareTo(super.get(0)) <= 0){
+        super.add(0, element);
+        System.out.println("Smallest");
         return true;
       }
-      super.add(element);
+      if(element.compareTo(super.get(super.size()-1)) >= 0){
+        super.add(element);
+        System.out.println("Biggest");
+        return true;
+      }
+      for(int i = 0; i < super.size()-1; i++){
+        T here = super.get(i);
+        T next = super.get(i+1);
+        if(element.compareTo(here) > 0 && element.compareTo(next) <= 0){
+          super.add(i+1, element);
+          i++;
+          System.out.println("Middling");
+          return true;
+        }
+
+      }
     }
+
     return false;
+  }
+
+  public void add(int index, T element){
+    if(element == null){
+      throw new IllegalArgumentException("null not allowed >:(");
+    }
+    if(super.size() == 0){
+      super.add(element);
+      return;
+    }
+
+
+    if(super.size() == 1){
+      if(element.compareTo(super.get(0)) >= 0){
+        super.add(element);
+        System.out.println(element);
+        return;
+      }
+      else{
+        super.add(0, element);
+        return;
+      }
+    }
+
+
+    if(super.size() > 1){
+
+      if(element.compareTo(super.get(0)) <= 0){
+        super.add(0, element);
+        return;
+      }
+      if(element.compareTo(super.get(super.size()-1)) >= 0){
+        super.add(element);
+        return;
+      }
+      for(int i = 0; i < super.size()-1; i++){
+        T here = super.get(i);
+        T next = super.get(i+1);
+        if(element.compareTo(here) > 0 && element.compareTo(next) <= 0){
+          super.add(i+1, element);
+          i++;
+          return;
+        }
+
+      }
+    }
   }
 }
